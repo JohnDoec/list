8ba05abbedd4d4a303402a39be588c7fa063d0ad
JohnDoe <zzy2096418031@gmail.com>
2024-09-06 23:01:40 +0800

    泛型的应用 2024年9月6日23:01:50

--- GitDomeList/src/views/HomeView.vue
+++ GitDomeList/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h2>TS 泛型的应用2024年9月6日23:01:17</h2>
+    {{ car }}
+
   </main>
 </template>
+
+
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+
+let car = reactive({ baoma: '宝马' });
+
+function student<T, T>(value: T, message: T) {
+  console.log(message)
+  return value;
+}
+
+console.log(student<number, string>(68, '小明'))
+
+</script>
